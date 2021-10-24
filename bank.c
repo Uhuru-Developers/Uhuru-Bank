de7e721fc6570354a2c964f680f92be802b74212
Ssemakula-Jonathan <uhurussemakula069@gmail.com>
2021-10-24 17:33:44 +0300

    Display account details

--- bank.c
+++ bank.c
@@ -1,6 +1,29 @@
 #include<stdio.h>
-
-void createAccount(char fname[], char lname[], char location[]){
+#include<string.h>
+void display(char sname[], char gname[], char date[], char district[], char village[], char contact[]){
+	char space[]=" ", stored2[100];
+	strcpy(stored2, strcat(sname, space));
+	printf("Name: ");
+	puts(strcat(stored2, gname));
+	
+	printf("\nDate of Birth: ");
+	puts(date);
+	
+	printf("\nAccount number: ");
+	puts(strcat(stored2, gname));
+	
+	printf("\nContact: ");
+	puts(contact);
+	
+	printf("\nCustomer ID: ");
+	puts(strcat(stored2, gname));
+	
+	printf("\nDistrict: ");
+	puts(district);
+	
+	printf("\nVillage: ");
+	puts(village);
+	
 }
 
 float accountBalance(float account){
@@ -8,5 +31,27 @@ float accountBalance(float account){
 }
 
 int main(){
+	char sname[20], gname[20], date[20], district[20], village[20], contact[20];
+	printf("Enter your surname: ");
+	gets(sname);
+	
+	printf("Enter your given name: ");
+	gets(gname);
+	
+	printf("Enter your date of birth: ");
+	gets(date);
+	
+	printf("Enter your district: ");
+	gets(district);
+	
+	printf("Enter your village: ");
+	gets(village);
+	
+	printf("Enter your contact: ");
+	gets(contact);
+	
+	display(sname, gname, date, district, village, contact);
 	
 }
+
+
